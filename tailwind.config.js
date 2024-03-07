83be6a43a8ef2ff713ca6472676b11fbf123061b
AnnaPrusakova <hanna.prusakova@gmail.com>
2024-03-07 13:42:05 +0200

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    colors: {
+      'gray': '#E0E4E5',
+      'accent': '#9195F6'
+    }
+  },
+  plugins: [],
+}
+
