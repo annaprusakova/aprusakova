eb1a9b7af447c694f21476f5e97213942065f793
AnnaPrusakova <hanna.prusakova@gmail.com>
2024-03-19 19:06:19 +0200

    add projects

---
+++ src/data/projects.js
@@ -0,0 +1,57 @@
+export const projects = [
+    {
+        id: 0, 
+        name: 'Notifications page', 
+        link: 'https://notifications-page-main-3ril.vercel.app/', 
+        image: 'https://raw.githubusercontent.com/annaprusakova/notifications-page-main/main/assets/img/desktop.png', 
+        description: 'Responsive notification page',
+        hashtags: ['React', 'TS', 'SASS']
+    }, 
+    {
+        id: 1, 
+        name: 'Suite landing page', 
+        link: 'https://candid-zabaione-3d96b2.netlify.app/', 
+        image: 'https://raw.githubusercontent.com/annaprusakova/suite-langing-page/main/src/assets/examples/Desktop-1.png', 
+        description: 'Responsive landing page',
+        hashtags: ['React', 'TS', 'Vite', 'SASS']
+    },
+    {
+        id: 2, 
+        name: 'Planets fact', 
+        link: 'https://planets-facts-sigma.vercel.app/planets/mercury', 
+        image: 'https://raw.githubusercontent.com/annaprusakova/planets-facts/main/public/desktop-1.png', description: 'The 8-page fact site for all planets in our solar system. ',
+        hashtags: ['React', 'TS', 'Next.js', 'SASS']
+    },
+    {
+        id: 3, 
+        name: 'Advice generator', 
+        link: 'https://advice-generator-app-main-snowy.vercel.app/', 
+        image: 'https://raw.githubusercontent.com/annaprusakova/advice-generator-app-main/main/public/desktop.png', 
+        description: 'This project uses the Advice Slip API to generate random quotes of advice.',
+        hashtags: ['React', 'TS', 'Next.js', 'Tailwind', 'Mobile-first workflow']
+    },
+    {
+        id: 4, 
+        name: 'Expenses chart component', 
+        link: 'https://expenses-chart-component-three-kappa.vercel.app/', 
+        image: 'https://raw.githubusercontent.com/annaprusakova/expenses-chart-component/main/public/desktop.png', 
+        description: 'Animated bar chart component',
+        hashtags: ['React', 'TS', 'Next.js', 'Tailwind', 'FramerMotion']
+    },
+    {
+        id: 5, 
+        name: 'Interactive comments', 
+        link: 'https://interactive-comments-pearl.vercel.app/', 
+        image: 'https://raw.githubusercontent.com/annaprusakova/interactive-comments/main/public/desktop-full.png', 
+        description: 'Responsive comment section',
+        hashtags: ['React', 'TS', 'Vite', 'Tailwind', 'Moment.js']
+    },
+    {
+        id: 6, 
+        name: 'NexterSeen', 
+        link: '', 
+        image: '', 
+        description: 'Movies and TV shows tracker. In process. Not live yet.',
+        hashtags: ['React', 'TS', 'Next.js', 'Tailwind', 'Node,js']
+    }
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ export default {
     extend: {},
     colors: {
       'gray': '#E0E4E5',
-      'accent': '#9195F6'
+      'dark-gray': '#898989',
+      'accent': '#9195F6',
+      'white': '#FFFFFF',
+      'pink': '#E178C5'
     }
   },
   plugins: [],
